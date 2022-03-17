9574ce1872eb9734e3c3cc550058eafa4f7a635b
Sibo Wang <sibo.wang@epfl.ch>
2022-03-17 14:46:14 +0100

    migrate PyPI projects to `nely-epfl` account

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="df2d",
+    name="nely-df2d",
     version="0.13",
     packages=["df2d"],
     author="Semih Gunel",
@@ -12,5 +12,5 @@ setup(
     description="2D pose estimation pipeline for tethered Drosophila.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/semihgunel/Drosophila2DPose",
+    url="https://github.com/NeLy-EPFL/Drosophila2DPose",
 )
