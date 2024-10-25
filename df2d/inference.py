4dc9aeb13d74b62eb4c60778e26ae786fc39e9cb
Dom <dominicd7@hotmail.com>
2024-10-25 16:30:12 +0200

    Don't output empty last frame See https://github.com/NeLy-EPFL/DeepFly3D/issues/57

--- df2d/inference.py
+++ df2d/inference.py
@@ -169,7 +169,7 @@ def inp2np(inp: List) -> np.ndarray:
     n_joints = inp[0][1].shape[0]
     n_dim = inp[0][1].shape[1]
 
-    points2d = np.zeros((n_cameras, n_images + 1, n_joints, n_dim))
+    points2d = np.zeros((n_cameras, n_images, n_joints, n_dim))
 
     for (path, pts) in inp:
         cid, imgid = parse_img_path(path)
